33af28bfa4f9d8b76241462c2a67b7cd540c4ce0
kenca23 <kenca23@gmail.com>
2020-12-29 22:08:41 -0600

    7_session-more-info

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -31,10 +31,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver { 
+    @Query(() => User, { nullable: true })
+    async me(@Ctx() { req, em }: MyContext) { 
+        // You are not logged in
+        if (!req.session.userId) { 
+            return null
+        }
+        const user = await em.findOne(User, { id: req.session.userId })
+        return user
+    }
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse>{ 
         if (options.username.length <= 2) {
             return {
@@ -73,6 +83,11 @@ export class UserResolver {
                 }
             }
         }
+
+        // store user id session
+        // this will set a cookie on the user
+        // keep them logged in
+        req.session.userId = user.id
         
         return {user}
     }
@@ -80,7 +95,7 @@ export class UserResolver {
     @Mutation(() => UserResponse)
     async login(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse> {
         const user = await em.findOne(User, {username: options.username})
         if (!user) { 
@@ -101,6 +116,7 @@ export class UserResolver {
             }
         }
         
+        req.session.userId = user.id
 
         return {user}
     }
